64277da5be469577e179d20f4443e981f267ebee
Petr Gadorek <petr.gadorek@gooddata.com>
2018-10-17 10:50:40 +0200

    minimal working version

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/presentation/index.html');
+});
+
+var server = http.createServer(app).listen(3000, function(){
+  console.log("Express server listening on port 3000");
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function (socket) {
+	// console.log('sending welcome')
+  socket.emit("message", "Welcome to Revealer");
+  socket.on("slidechanged", function(data){
+		// console.log('got: ',data);
+		socket.broadcast.emit("slidechanged", data);
+	});
+});
+
